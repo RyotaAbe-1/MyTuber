a540ce4a8889fb55253cafe376492736d919f3e4
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-11 10:47:30 +0000

    [Add]adminジャンル登録画面にジャンル作成フォーム追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,16 @@
 class Admin::GenresController < ApplicationController
   def new
-    @genres = Genre.where(application_status: false)
+    @genres = Genre.where(application_status: true)
     @genre = Genre.new
+  end
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to new_admin_genre_path
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:genre_name)
   end
 end
