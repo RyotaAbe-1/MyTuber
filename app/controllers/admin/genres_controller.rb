43128dd3d8ff88ac4c2aaeab60e45c24d44f200a
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-11 11:06:34 +0000

    [Add]adminジャンル登録画面のジャンル追加機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,16 +1,23 @@
 class Admin::GenresController < ApplicationController
   def new
-    @genres = Genre.where(application_status: true)
+    @genres = Genre.where(application_status: false)
     @genre = Genre.new
   end
+
   def create
     genre = Genre.new(genre_params)
     genre.save
     redirect_to new_admin_genre_path
   end
 
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to new_admin_genre_path
+  end
+
   private
   def genre_params
-    params.require(:genre).permit(:genre_name)
+    params.require(:genre).permit(:genre_name, :application_status)
   end
 end
