fc631cdadda009d9a0dbb89a0084dec91fe267dd
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-11 06:08:21 +0000

    [Add]admin,publicのログイン後遷移設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if admin_signed_in?
+      new_admin_genre_path
+    elsif user_signed_in?
+      root_path
+    end
+  end
 end
