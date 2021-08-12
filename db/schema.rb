c809c983cf727432ffa2e7cb9a93b527de45e299
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-12 10:22:54 +0000

    [Fix]usersテーブルにuser_nameカラムとimage_idカラムが作成されていない問題を修正

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(version: 2021_08_10_054424) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "user_name", null: false
+    t.string "image_id", null: false
+    t.text "introduce", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
