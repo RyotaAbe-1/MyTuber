72616c12a812e90f1c4bcb2094fbc234a89227e2
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-15 10:05:47 +0000

    [Remove]youtuber_genreモデル

--- app/models/youtuber.rb
+++ app/models/youtuber.rb
@@ -1,6 +1,6 @@
 class Youtuber < ApplicationRecord
   has_many :favorites,       dependent: :destroy
-  has_many :youtuber_genres, dependent: :destroy
   has_many :comments,        dependent: :destroy
   belongs_to :user
+  belongs_to :
 end
