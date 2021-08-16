ecb753cf4c3ec9924ab277a6611ecacd7edc6044
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-16 12:20:53 +0000

    [Add]public投稿編集機能

--- app/controllers/public/youtubers_controller.rb
+++ app/controllers/public/youtubers_controller.rb
@@ -4,14 +4,26 @@ class Public::YoutubersController < ApplicationController
     @user_profile = User.find(current_user.id)
     @genres = Genre.where(application_status: true)
   end
-  
+
   def create
     youtuber = Youtuber.new(youtuber_params)
     youtuber.user_id = current_user.id
     youtuber.save
     redirect_to youtuber_path(youtuber)
   end
-  
+
+  def edit
+    @youtuber = Youtuber.find(params[:id])
+    @user_profile = User.find(current_user.id)
+    @genres = Genre.where(application_status: true)
+  end
+
+  def update
+    youtuber = Youtuber.find(params[:id])
+    youtuber.update(youtuber_params)
+    redirect_to youtuber_path(youtuber)
+  end
+
   private
   def youtuber_params
     params.require(:youtuber).permit(:genre_id, :name, :evaluation, :content)
