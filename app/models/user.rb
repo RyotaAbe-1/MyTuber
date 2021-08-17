a585d7b5b1f7885f89b51ab91b0ebf8b9d5751a8
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-17 11:38:03 +0000

    [Add]Relationshipモデル作成と関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   has_many :genres, through: :user_genres
   has_many :favorites
   has_many :youtubers
-  has_many :follows
   has_many :comments
+  has_many :follow_relationships, foreign_key: "follow_id", class_name: "Relationship", dependent: :destroy
+  has_many :follows, through: :follow_relationships
+  has_many :follower_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
 end
