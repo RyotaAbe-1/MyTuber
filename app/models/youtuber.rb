3511e85dc43c3eef6117e41252884d7d3635df36
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-18 15:34:18 +0000

    [Fix]コメント数をお気に入り数と同じ書式に変更

--- app/models/youtuber.rb
+++ app/models/youtuber.rb
@@ -3,7 +3,11 @@ class Youtuber < ApplicationRecord
   has_many :comments,        dependent: :destroy
   belongs_to :user
   belongs_to :genre
-
+  
+  def number_of_comments
+    comments.count
+  end
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
@@ -11,5 +15,5 @@ class Youtuber < ApplicationRecord
   def number_of_favorites
     favorites.count
   end
-
+  
 end
