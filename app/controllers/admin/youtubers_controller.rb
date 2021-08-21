03647426e3680107aa7ed5bc42525778a9f308a2
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-21 10:48:50 +0000

    [Add]admin投稿一覧画面

--- app/controllers/admin/youtubers_controller.rb
+++ app/controllers/admin/youtubers_controller.rb
@@ -1,2 +1,12 @@
 class Admin::YoutubersController < ApplicationController
+  def index
+    @youtubers = Youtuber.all.order("created_at DESC")
+    @genres = Genre.where(application_status: true)
+    sort = params[:sort]
+    if sort == "genre-search"
+      @genre = Genre.find(params[:genre_id])
+    elsif sort == "search"
+      @youtubers = Youtuber.search(params[:keyword]).order("created_at DESC")
+    end
+  end
 end
