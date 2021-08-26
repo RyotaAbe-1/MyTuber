7a0d3b95ce6143967b7e79323624f3876b71c001
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-26 23:30:16 +0000

    [Add]admin検索後表示ページのN+1問題対策

--- app/controllers/admin/youtubers_controller.rb
+++ app/controllers/admin/youtubers_controller.rb
@@ -5,8 +5,6 @@ class Admin::YoutubersController < ApplicationController
     sort = params[:sort]
     if sort == "genre-search"
       @genre = Genre.find(params[:genre_id])
-    elsif sort == "search"
-      @youtubers = Youtuber.search(params[:keyword]).order("created_at DESC")
     end
   end
 
