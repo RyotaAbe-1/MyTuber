548ef7676ccfb585c6eb61d02e4e3826635f5956
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-08-26 23:08:48 +0000

    [Add]admin投稿一覧のN+1問題対策

--- app/controllers/admin/youtubers_controller.rb
+++ app/controllers/admin/youtubers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::YoutubersController < ApplicationController
   def index
-    @youtubers = Youtuber.all.order("created_at DESC")
+    @youtubers = Youtuber.all.order("created_at DESC").includes(:user, :genre, :comments, :favorites)
     @genres = Genre.where(application_status: true)
     sort = params[:sort]
     if sort == "genre-search"
@@ -9,16 +9,16 @@ class Admin::YoutubersController < ApplicationController
       @youtubers = Youtuber.search(params[:keyword]).order("created_at DESC")
     end
   end
-  
+
   def show
     @youtuber = Youtuber.find(params[:id])
     @user_profile = User.find(@youtuber.user.id)
     @genres = Genre.where(application_status: true)
   end
-  
+
   def destroy
     youtuber = Youtuber.find(params[:id])
     youtuber.destroy
     redirect_to admin_youtubers_path
-  end  
+  end
 end
