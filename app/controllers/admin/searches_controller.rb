63fbf7c68530cffe7672768cdb45ed9bb4663a93
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-09-03 14:13:29 +0000

    [Fix]adminの検索時にkaminariが適応されていなかった問題を修正

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -2,11 +2,11 @@ class Admin::SearchesController < ApplicationController
   def search
     category = params[:category]
     if category == "search-by-user"
-      @users = User.search(params[:keyword]).includes(:genres).order("created_at DESC")
+      @users = User.search(params[:keyword]).page(params[:page]).includes(:genres).order("created_at DESC")
       @genres = Genre.where(application_status: true)
       render "admin/users/index"
     elsif category == "search-by-post"
-      @youtubers = Youtuber.search(params[:keyword]).page(params[:page]).includes(:user, :genre, :comments, :favorites).includes(:user, :genre, :comments, :favorites).order("created_at DESC")
+      @youtubers = Youtuber.search(params[:keyword]).page(params[:page]).per(10).includes(:user, :genre, :comments, :favorites).order("created_at DESC")
       @genres = Genre.where(application_status: true)
       render "admin/youtubers/index"
     end
