f145b478abb29edefa2fa65ca0b43335647cb14e
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-09-04 12:59:20 +0000

    [Fix]プロフィールバーのフォローボタンのコントローラーのロジックを簡略化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -4,24 +4,20 @@ class Public::RelationshipsController < ApplicationController
   def follow
     current_user.follow(params[:id])
     page = params[:page]
-    if page == "user-index"
-      @user = User.find(params[:id])
-    elsif (page == "user-show") || (page == "post-show")
-      @user = User.find(params[:id])
-    elsif page == "post-index"
+    if page == "post-index"
       @youtuber = Youtuber.find(params[:youtuber_id])
+    else
+      @user = User.find(params[:id])
     end
   end
 
   def unfollow
     current_user.unfollow(params[:id])
     page = params[:page]
-    if page == "user-index"
-      @user = User.find(params[:id])
-    elsif (page == "user-show") || (page == "post-show")
-      @user = User.find(params[:id])
-    elsif page == "post-index"
+    if page == "post-index"
       @youtuber = Youtuber.find(params[:youtuber_id])
+    else
+      @user = User.find(params[:id])
     end
   end
 end
