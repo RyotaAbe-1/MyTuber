6762698eeadb28a9952298f24fd966d4d7e787be
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-09-04 10:23:14 +0000

    [Fix]public投稿詳細画面のコメントフォームのvalidationメッセージがうまく表示されない問題を修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,7 +2,7 @@ class Public::CommentsController < ApplicationController
   skip_before_action :authenticate_admin!
   include CommonActions
   before_action :set_genres, only: [:create]
-  
+
   def create
     @youtuber = Youtuber.find(params[:youtuber_id])
     @comment = Comment.new(comment_params)
@@ -11,8 +11,10 @@ class Public::CommentsController < ApplicationController
     if @comment.save
       redirect_to youtuber_path(@youtuber)
     else
-      @youtuber_comments = @youtuber.comments.includes(:user)
+      @youtuber_comments = @youtuber.comments.page(params[:page]).includes(:user)
       @user_profile = User.find(@youtuber.user.id)
+      genre_ids = @user_profile.user_genres.select(:genre_id)
+      @common_genres = current_user.genres.where(id: genre_ids)
       render "public/youtubers/show"
     end
   end
