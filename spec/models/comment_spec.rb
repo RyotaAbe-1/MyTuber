331643af2fdf203dcec08f19accd39873a38fe43
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-09-05 14:49:18 +0000

    [Fix]commentのモデルテストのマッチャーを修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe Comment, "モデルに関するテスト" , type: :model do
     context "空白のバリデーションチェック" do
       it "contentが空白の場合にエラーメッセージが返ってきているか" do
         comment = build(:comment, content: "")
-        expect(comment).to be_valid
+        expect(comment).to be_invalid
         expect(comment.errors[:content]).to include("を入力してください")
       end
     end
     context "字数制限のバリデーションチェック" do
       it "contentが200文字を超える場合にエラーメッセージが返ってきているか" do
         comment = build(:comment, content: Faker::Lorem.characters(number: 101))
-        expect(comment).to be_valid
+        expect(comment).to be_invalid
         expect(comment.errors[:content]).to include("は100文字以内で入力してください")
       end
     end
