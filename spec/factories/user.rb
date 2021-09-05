f58e7b813ec216dcd147879761c635bf792da2c7
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-09-05 13:41:06 +0000

    [Update]FactoryBotのテストデータの定義を変更

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     user_name { Faker::Lorem.characters(number:12) }
-    email { Faker::Lorem.characters(number:20) }
+    email { "#{Faker::Lorem.characters(number:10)}@gmail.com"}
     password = Faker::Internet.password(min_length: 6)
     password { password }
     password_confirmation { password }
