c5f28cb0c43056919f0bf04d315da61fc83588f5
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-10-05 10:34:10 +0000

    [Add]お問い合わせフォーム入力画面のlayout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
       port: 587,
       address: "smtp.gmail.com",
       domain: "smtp.gmail.com",
+      authentication: 'plain',
       user_name: ENV['SMTP_USERNAME'],
       password: ENV['SMTP_PASSWORD'],
       enable_starttls_auto: true
